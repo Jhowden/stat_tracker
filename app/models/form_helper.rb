2384f040acf673f11ef0355343727bbac03f0860
Jonathan Howden <jhowden@backstopsolutions.com>
2015-12-23 18:09:18 -0600

    Allowing user to add multiple goals to a game

--- app/models/form_helper.rb
+++ app/models/form_helper.rb
@@ -1,7 +1,7 @@
 class FormHelper
   extend Sinatra::FormHelpers, Sinatra::Templates
   
-  def self.link_to_add_fields(name, fields )
+  def self.link_to_add_fields( name, fields )
     link( name, '#', class: "add_fields", data: fields.gsub( "\n", "" ) )
   end
 end
